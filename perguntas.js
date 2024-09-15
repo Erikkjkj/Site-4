fb4708e7d7ba3c29a2f6d024a6df39fd820c905b
Erikkjkj <159954567+Erikkjkj@users.noreply.github.com>
2024-09-15 19:25:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'História',
+    'Quando começou e terminou a primeira Guerra?',
+    'Em julho de 1914 a novembro de 1918'
+)
+
+criaCartao(
+    'História',
+    'Quais foram os principais países envolvidos na Grande Guerra?',
+    'Aliança entre França, Reino Unido, Rússia.'
+)
+
+criaCartao(
+    'História',
+    'O que era a Tríplice Entente "aliados"?',
+    'Aliança entre França, Reino Unido, Rússia.'
+)
+
+criaCartao(
+    'História',
+    'O que era a tríplice Aliança "Países/Impérios Centrais"?',
+    'Aliança entre a Alemanha, Itália, Império Áustro-Húngaro'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o stopin da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando em Sarajevo, Bósnia.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a "guerra de trincheiras',
+    'Foi um tipo de guerra onde os soldados lutavam em redes de trincheiras escavadas no solo, especialmente na Frente Ocidental, levando a batalhas longas e mortais.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o impacto das novas tecnologias na guerra?',
+    'Tecnologias como metralhadoras, tanques, aviões e armas químicas tornaram a guerra mais mortal e difícil para os soldados.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o papel dos Estados Unidos na Primeira Guerra Mundial?',
+    'Os EUA entraram na guerra em 1917, ajudando a virar o jogo a favor da Tríplice Entente com seu poder econômico e militar.'
+)
+
+criaCartao(
+    'História',
+    'O que foi o Tratado de Versalhes',
+    'Foi o tratado de paz assinado em 1919 que encerrou oficialmente a guerra, impondo duras condições à Alemanha.'
+)
+
+criaCartao(
+    'História',
+    'O que foi o Tratado de Versalhes',
+    'Foi o tratado de paz assinado em 1919 que encerrou oficialmente a guerra, impondo duras condições à Alemanha.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Liga das Nações?',
+    'Foi uma organização internacional criada após a guerra para promover a paz e evitar novos conflitos, mas falhou em seu objetivo.'
+)
+
+criaCartao(
+    'História',
+    'Quais foram as principais consequências da Primeira Guerra Mundial?',
+    'A guerra causou a morte de milhões, destruição econômica e o colapso de impérios, além de preparar o cenário para a Segunda Guerra Mundial.'
+)
